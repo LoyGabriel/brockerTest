5d0579178117df9914a8bdb191d4350d89b75170
Gabriel Loy <gabrielloy01@gmail.com>
2021-01-14 23:02:50 -0300

    First Commit

---
+++ broker-test.zip_expanded/broker-test/src/main/java/com/gloy/brokertest/BrokerTestApplication.java
@@ -0,0 +1,13 @@
+package com.gloy.brokertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerTestApplication.class, args);
+	}
+
+}
